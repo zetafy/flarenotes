a312821573b3833cc93f2e4e17bdce7d4bf1c46a
Icey <iceyvision01@gmail.com>
2024-02-14 12:39:50 +1100

    Added: Login and signup tabs, /login route

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,5 +1,85 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Separator } from "@/components/ui/separator"
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import { ImGoogle } from "react-icons/im"
+
 export default function LoginPage() {
-    return (
-        <h1>Login Page</h1>
-    )
+  return (
+    <div className="flex justify-center mt-5">
+        <Tabs defaultValue="Login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="Login">Login</TabsTrigger>
+            <TabsTrigger value="Signup">Sign up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="Login">
+            <Card>
+            <CardHeader>
+                <Button><ImGoogle className="h-4 w-4 mr-3"/>Login with Google</Button>
+                <Button><GitHubLogoIcon className="h-4 w-4 mr-3" />Login with Github</Button>
+                </CardHeader>
+                <Separator className="mb-4"/>
+            <CardContent className="space-y-2">
+            <CardTitle>Login</CardTitle>
+                <CardDescription>
+                Enter your login details, or login with available options below.
+                </CardDescription>
+                <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Enter your username" />
+                </div>
+                <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="Enter your password" type="password" />
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button>Login</Button>
+            </CardFooter>
+            </Card>
+        </TabsContent>
+        <TabsContent value="Signup">
+            <Card>
+                <CardHeader>
+                    <Button><ImGoogle className="h-4 w-4 mr-3"/>Signup with Google</Button>
+                    <Button><GitHubLogoIcon className="h-4 w-4 mr-3" />Singup with Github</Button>
+                </CardHeader>
+                <Separator className="mb-4"/>
+            <CardContent className="space-y-2">
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                Sign up for a Flarenotes account here.
+                </CardDescription>
+                <div className="space-y-1">
+                <Label htmlFor="signup-username">Username</Label>
+                <Input id="signup-username" placeholder="Create a username" />
+                </div>
+                <div className="space-y-1">
+                <Label htmlFor="signup-password">Password</Label>
+                <Input id="signup-password" type="password" placeholder="Create a strong password" />
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button>Sign Up</Button>
+            </CardFooter>
+            </Card>
+        </TabsContent>
+        </Tabs>
+    </div>
+  )
 }
