e6690ed321cade0ac5ee88fe3ac613baf8537e8b
modithebest <hardikchojar2017@gmail.com>
2024-02-15 12:31:54 +1100

    Linking github icon to repository & replacing features with browse notes

--- client/components/layout/navbar.tsx
+++ client/components/layout/navbar.tsx
@@ -30,15 +30,9 @@ export default function Navbar() {
       <nav className="mx-auto justify-between px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
         <div>
           <div className="flex items-center justify-between py-3 md:block md:py-5">
-<<<<<<< HEAD
             <Link href="/" onClick={handleClick} className="flex items-center gap-2">
-              <Image src="/logo.png" width={25} height={25} alt="logo.png" className="items-start" />
+              <Image src="/logo.png" width={25} height={25} alt="logo.png" className="items-start hover:scale-105" />
               <h1 className="text-2xl font-bold bg-clip-text text-black hover:text-transparent hover:bg-gradient-to-r from-red-900 to-orange-600 duration-300 ease-in-out lg:hover:scale-110 dark:text-white">
-=======
-            <Link href="/" onClick={handleClick} className="flex items-center gap-2 hover:gap-4">
-              <Image src="/logo.png" width={23} height={23} alt="logo.png" className="items-start" />
-              <h1 className="text-2xl font-bold bg-clip-text text-black hover:text-transparent hover:bg-gradient-to-r from-red-900 to-orange-600 duration-300 ease-in-out lg:hover:scale-110">
->>>>>>> b68bed8404cad629d88a88125a54c95797c6ae43
                 {siteConfig.name}
               </h1>
             </Link>
@@ -81,9 +75,15 @@ export default function Navbar() {
                 )}
               </button>
               <div className="flex">
-                <Button variant="outline" size="icon">
-                  <GitHubLogoIcon />
-                </Button>
+                <Link
+                  href={"https://github.com/zetafy/flarenotes"}
+                  style={{ display: "flex" }}
+                  target="_blank"
+                >
+                  <Button variant="outline" size="icon" className="Github-icon">
+                    <GitHubLogoIcon />
+                  </Button>
+                </Link>
                 <ModeToggle />
               </div>
             </div>
@@ -113,9 +113,15 @@ export default function Navbar() {
         {settings.themeToggleEnabled && (
           <div className="hidden md:block">
             <div className="flex">
-              <Button variant="outline" size="icon">
-                <GitHubLogoIcon />
-              </Button>
+              <Link
+                href={"https://github.com/zetafy/flarenotes"}
+                style={{ display: "flex" }}
+                target="_blank"
+              >
+                <Button variant="outline" size="icon">
+                  <GitHubLogoIcon />
+                </Button>
+              </Link>
               <ModeToggle />
             </div>
           </div>
