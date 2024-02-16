bc53ac5976ea86eeebc9e6e0cebd1a4192e239db
Icey <iceyvision01@gmail.com>
2024-02-16 15:46:45 +1100

    Updated footer to remove large margin

--- client/components/layout/footer.tsx
+++ client/components/layout/footer.tsx
@@ -5,9 +5,9 @@ import { navLinks } from "@/lib/links"
 export default function Footer() {
   return (
     <footer className="mt-auto">
-      <div className="mx-auto w-full max-w-screen-xl p-6 md:py-8">
+      <div className="mx-auto w-full max-w-screen-xl p-6 pt-0">
         
-        <hr className="my-6 text-muted-foreground sm:mx-auto lg:my-8" />
+        <hr className="my-3 text-muted-foreground sm:mx-auto" />
         <span className="block text-sm text-muted-foreground sm:text-center">
           © {new Date().getFullYear()}{" "}
           <a
