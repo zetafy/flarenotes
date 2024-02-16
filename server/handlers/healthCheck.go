cb4d9536a4ab618712ac9eada256e311eb35960c
abyanmajid <abyan@abydyl.net>
2024-02-16 15:26:46 +1100

    add: chat crud operations

--- server/handlers/healthCheck.go
+++ server/handlers/healthCheck.go
@@ -5,7 +5,40 @@ import (
 )
 
 func HealthCheck(context *gin.Context) {
-	context.JSON(200, gin.H{
-		"message": "Server is running!",
-	})
+	helpMessage := `
+███████╗██╗      █████╗ ██████╗ ███████╗███╗   ██╗ ██████╗ ████████╗███████╗███████╗
+██╔════╝██║     ██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔═══██╗╚══██╔══╝██╔════╝██╔════╝
+█████╗  ██║     ███████║██████╔╝█████╗  ██╔██╗ ██║██║   ██║   ██║   █████╗  ███████╗
+██╔══╝  ██║     ██╔══██║██╔══██╗██╔══╝  ██║╚██╗██║██║   ██║   ██║   ██╔══╝  ╚════██║
+██║     ███████╗██║  ██║██║  ██║███████╗██║ ╚████║╚██████╔╝   ██║   ███████╗███████║
+╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝
+
+Flarenotes API v0.1.0
+
+(GET: /users) Fetch all users
+(GET: /users/:id) Fetch user by id
+(POST: /users) Create user
+(PATCH: /users/:id) Update user by id
+(DELETE: /users/:id) Delete user by id
+
+(GET: /notebooks) Fetch all notebooks
+(GET: /notebooks/:id) Fetch notebook by id
+(POST: /notebooks) Create notebook
+(PATCH: /notebooks/:id) Update notebook by id
+(DELETE: /notebooks/:id) Delete notebook by id
+
+(GET: /notes) Fetch all notes
+(GET: /notes/:id) Fetch note by id
+(POST: /notes) Create note
+(PATCH: /notes/:id) Update note id
+(DELETE: /notes/:id) Delete note by id
+
+(GET: /chats) Fetch all chats
+(GET: /chats/:id) Fetch chat by id
+(POST: /chats) Create chat
+(PATCH: /chats/:id) Update chat by id
+(DELETE: /chats/:id) Delete chat by id
+
+`
+	context.String(200, helpMessage)
 }
